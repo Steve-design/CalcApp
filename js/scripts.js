d365d46436d7b9e29c7ddfff8f6b81ed5de2f6fd
Steve-design <smzalendo31@gmail.com>
2019-08-19 11:30:45 +0300

    add background image to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -21,9 +21,7 @@ function validate() {
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
     var gender = document.getElementsByName("gender1");
-    var i = 0;
 
-   
   
     if (day == "" || day == null) {
       alert("Day is required");
@@ -33,6 +31,7 @@ function validate() {
           alert("Day is invalid");
         } else {
           alert(day);
+          
         }
       } else {
         alert("Number required");
@@ -48,6 +47,7 @@ function validate() {
           
         } else {
           alert(month);
+          
         }
       } else {
         alert("Number required");
@@ -63,11 +63,13 @@ function validate() {
           alert("Year is invalid");
         } else {
           alert(year);
+          
         }
       } else {
         alert("Number required");
       }
     }
+  
    
     if(gender[0].checked===false && gender[1].checked ===false){
         alert("Select gender");
@@ -75,55 +77,52 @@ function validate() {
         for(i=0;i<gender.length; i++){
             if(gender[i].checked){
                 alert(gender[i].value);
-                var details={
-                  day:day,
-                  month:month,
-                  year:year,
-                  gender:gender
+                var gend = gender[i].value;
+                //return gend;
+                console.log(gend);
+              
                 }
-            }
+               
         }
-    }
+    
+  }
+}
+  
+
     function myFunction(){
       var valid = validate();
-      var d = parseInt(valid.day);
-      var m = parseInt(valid.month);
-      var year = valid.year;
-      var gender = valid.gender;
+      var d = parseInt(valid.mDay);
+      var m = parseInt(valid.mMonth);
+      var year = valid.mYear;
+      var gender = valid.mGender;
  
- var a = Math.floor((14 - m) / 12);
- var y = year - a;
-  var m = m + 12 * a - 2;
-  var dayOfTheWeek = (d + y + Math.floor(y / 4) - Math.floor(y / 100) +
-  Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
-  console.log(dayOfTheWeek);
-  var daysOfaweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',       
-          'Thursday', 'Friday', 'Saturday');
+//  var a = Math.floor((14 - m) / 12);
+//  var y = year - a;
+//   var m = m + 12 * a - 2;
+//   var dayOfTheWeek = (d + y + Math.floor(y / 4) - Math.floor(y / 100) +
+//   Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
+//   console.log(dayOfTheWeek);
+//   var daysOfaweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',       
+//           'Thursday', 'Friday', 'Saturday');
   
- var myDay = daysOfaweek[dayOfTheWeek];
- if (gender === "male") {
- for (var k in maleAkan) {
-         if (maleAkan.hasOwnProperty(k)) {
-             if (k === myDay) {
+//  var myDay = daysOfaweek[dayOfTheWeek];
+//  if (gender === male) {
+//  for (var k in maleAkan) {
+//          if (maleAkan.hasOwnProperty(k)) {
+//              if (k === myDay) {
                  
-                 alert("Bingo! Your Akan name is "+maleAkan[k]);
-             }
-         }
-     }
-  }else if(gender === "female") {
-     for (var k in femaleAkan) {
-             if (femaleAkan.hasOwnProperty(k)) {
-                 if (k === myDay){
-                     alert("Bingo! Your Akan name is "+femaleAkan[k]);
-                 }
-             }
-         }
+//                  alert("Bingo! Your Akan name is "+maleAkan[k]);
+//              }
+//          }
+//      }
+//   }else if(gender === female) {
+//      for (var k in femaleAkan) {
+//              if (femaleAkan.hasOwnProperty(k)) {
+//                  if (k === myDay){
+//                      alert("Bingo! Your Akan name is "+femaleAkan[k]);
+//                  }
+//              }
+//          }
      
-      }
- }
- 
- 
- 
- 
-
-}
+//       }
+    }
